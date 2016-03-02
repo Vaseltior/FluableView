6ec1030a887538a9292eee6711d9995dfca20fe0
Samuel GRAU <samuel.grau@gmail.com>
2016-03-02 19:40:47 +0100

    Initial Commit

---
+++ FluableView/FluableView.h
@@ -0,0 +1,19 @@
+//
+//  FluableView.h
+//  FluableView
+//
+//  Created by Samuel Grau on 02/03/2016.
+//  Copyright © 2016 Samuel GRAU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FluableView.
+FOUNDATION_EXPORT double FluableViewVersionNumber;
+
+//! Project version string for FluableView.
+FOUNDATION_EXPORT const unsigned char FluableViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FluableView/PublicHeader.h>
+
+
